877bbbfc729f5ac4b42dce034e04f1a3cf249ed6
JotsoChas <joco.borghol@chasacademy.se>
2025-09-17 22:25:48 +0200

    Add project files.

---
+++ OOP Class Person/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Class_Person
+{
+    public class Person
+    {
+       
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int BirthYear { get; set; }
+        public int GetAge()
+        {
+            int currentYear = DateTime.Now.Year;
+            return currentYear - BirthYear;
+        }
+
+
+
+        public Person(string firstname, string lastname, int birthyear)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            BirthYear = birthyear;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{FirstName} {LastName} och är född {BirthYear} och är {GetAge()} år gammal");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ OOP Class Person/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OOP_Class_Person
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myPerson = new Person("Joco", "Borghol", 1988);
+            myPerson.PrintInfo();   
+        }
+    }
+}
